bec6a39caf289b7bba79ad130be7d3eac9888490
f20180864 <42579396+f20180864@users.noreply.github.com>
2018-09-29 11:47:23 +0530

    Update and rename final2 to final2.ino

--- final2.ino
+++ final2.ino
@@ -1,5 +1,6 @@
 #include <String.h>
-
+int ledpin = 2;
+int spin = 3;
 int rf = 12;
 int rb = 10;
 int lf = 9;
@@ -9,8 +10,8 @@ int re=5;
 int le=6;
 int rt=8;
 int lt=7;
-
-int a[5], e, v=150, lae=0, j=0, de=30, turnde=200, ts=200, reliefde=750;
+bool drun= LOW,lastbutoon = LOW;
+int a[5], e, v=100, lae=0, j=0, de=80, turnde=275, ts=200, reliefde=75;
 void setup()
 { Serial.begin(9600);
   pinMode(rf,OUTPUT);
@@ -20,12 +21,13 @@ void setup()
   pinMode(lf, OUTPUT);
   pinMode(lb, OUTPUT);
   pinMode(4, OUTPUT);
-
+  pinMode(spin,INPUT);
+  pinMode(ledpin,OUTPUT);
   digitalWrite(4, HIGH);
 }
 
 void straight(int dv1){    //arguments are for dv(error)
-  Serial.println(dv1+v);
+ 
   
   analogWrite(re,v-dv1);
   analogWrite(le,v+dv1);
@@ -73,6 +75,17 @@ void hardLeft()                          //90 degree left active at turns only
   stp();
   
 }
+void uturn()
+{
+  analogWrite(re,ts);
+  analogWrite(le,ts);
+  digitalWrite(lb,HIGH);
+  digitalWrite(rf,HIGH);
+  digitalWrite(rb,LOW);
+  digitalWrite(lf,LOW);
+  delay(2*turnde);
+  stp();  
+}
 int reading()                          // takes reading from sensors and converts it into a single value and returns to the main function
 {int ledNo=0;
 int val = 0;
@@ -111,37 +124,67 @@ a[4]=!digitalRead(A4);
 }
 int correction(int x)                 // damps dv and returns to main function
 {int y;
-e=35-x;
+e=30-x;
 y=2*e+0.9*(e-lae);
 lae=e;
 return y;
  }
-
+void ledglow()
+{
+  while(digitalRead(rt) == HIGH && digitalRead(lt) == HIGH)
+  {
+    drun = HIGH;
+    digitalWrite(ledpin, HIGH);
+    delay(50);
+    digitalWrite(ledpin, LOW);
+    delay(50);
+  }
+  drun = LOW;
+}
 unsigned long tpassed;
 
 void loop()
-{ 
+{
   int val=reading();
-
+  Serial.print(path);
   if(val>9 && val<51){                                    // bot is deviated 
     int dv3 = correction(val);
     straight(dv3);
+  }  else if(val==0 ){
+    stp();
+    /*uturn;
+    tpassed = millis();
+    while((millis()-tpassed)<reliefde){ straight(correction(reading())); delay(5);}
+    */
   }
-  else if(val==0){ stp(); }
   
-  if(digitalRead(rt)==HIGH){                             //check reading function to understand this condition
+  if(digitalRead(rt)==HIGH && digitalRead(lt)==HIGH)
+  {
     delay(de);
-    if(digitalRead(A0)==LOW){
+    if(digitalRead(rt)==HIGH && digitalRead(lt)==HIGH)
+    {
+      stp();
+     ledglow();
+    }else{
+      hardLeft();
+    tpassed = millis();
+    while((millis()-tpassed)<reliefde){ straight(correction(reading())); delay(5);}
+   }
+  }
+  else if(digitalRead(rt)==HIGH && digitalRead(lt)==LOW){                             //check reading function to understand this condition
+      delay(de);
+    if(digitalRead(A2)==LOW)
+    {
       path[j] = 's';
       j++;
-      // go straight
-     }
-    // else{ hardRight(); }
+    }else if(digitalRead(A2) == HIGH)
+    {
     hardRight(); 
     tpassed = millis();
     while((millis()-tpassed)<reliefde){ straight(correction(reading())); delay(5);}
+    } 
   }
-  else if(digitalRead(lt)==HIGH){
+  else if(digitalRead(lt)==HIGH && digitalRead(rt)==LOW){
     delay(de);
     if(digitalRead(A0)==LOW)
     {
@@ -153,5 +196,7 @@ void loop()
     while((millis()-tpassed)<reliefde){ straight(correction(reading())); delay(5);}
   }
   delay(10);
+
+
 }
   
